ac68c97afad2d06b716d1b765a1d84df2fada8f1
Kunal Jindal <kunal.jindal@hotstar.com>
2022-10-18 10:16:30 +0530

    Added API for personalized hello message.

--- src/main/java/com/example/hellohotstar/HomeController.java
+++ src/main/java/com/example/hellohotstar/HomeController.java
@@ -2,6 +2,7 @@ package com.example.hellohotstar;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +13,11 @@ public class HomeController {
     public String sayHello(){
 
         return "Hello Hotstar!";
+    }
+
+    @RequestMapping("/hello/{name}")
+    public String sayHelloWithName(@PathVariable("name") String name){
+
+        return "Hello " + name + ". Welcome to hotstar";
     }
 }
