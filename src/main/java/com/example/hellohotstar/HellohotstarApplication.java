74e491a46070c7b57a11472b11118d5f708b6437
Kunal Jindal <kunal.jindal@hotstar.com>
2022-10-18 09:57:35 +0530

    Added hellohotstar springboot app.

---
+++ src/main/java/com/example/hellohotstar/HellohotstarApplication.java
@@ -0,0 +1,13 @@
+package com.example.hellohotstar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellohotstarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HellohotstarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hellohotstar/HellohotstarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hellohotstar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellohotstarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
